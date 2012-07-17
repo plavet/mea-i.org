ff6a0e164a417ac3741e484a36adff630f8d8426
Igor Boskovic <plavetworld@gmail.com>
2012-07-17 14:14:47 +0200

    ver.1.0.
    initial setup

---
+++ wp-content/themes/mea-i/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content
+ * after.  Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers 3.0
+ */
+?>
+<footer>
+<?php get_sidebar( 'footer' ); ?>
+
+			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
+
+<?php wp_footer(); ?>
+</footer>
+</div><!--/wrap-->
+</body>
+</html>
